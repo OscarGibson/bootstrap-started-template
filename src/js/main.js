f29870c7ddafebc7ae4935f84b01ad5f4e979f80
ostap <oneostap@gmail.com>
2018-05-29 00:12:46 +0300

    create post page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
+function() {
+	console.log(window.innerWidth + " X " + window.innerHeight);
+
+	let navHeight = $('.navbar').outerHeight();
+
+	console.log(navHeight);
+
+	$(".full-size").css({
+		height : window.innerHeight - navHeight
+	});
+}();
+
+
 $(document).ready(function() {
 
-    // Js code here..
+    // code here...
 
 });
---
+++ src/js/partials/fullSize.js
@@ -0,0 +1,9 @@
+console.log(window.innerWidth + " X " + window.innerHeight);
+
+let navHeight = $('.navbar').outerHeight();
+
+console.log(navHeight);
+
+$(".full-size").css({
+	height : window.innerHeight - navHeight
+});
